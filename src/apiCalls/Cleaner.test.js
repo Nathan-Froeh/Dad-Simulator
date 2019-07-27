2e200ae490067f9e15f406de214cfd3994f6c709
Nathan <nathan.froeh@gmail.com>
2019-07-27 15:19:10 -0600

    Add Cleaner tests

--- src/apiCalls/Cleaner.test.js
+++ src/apiCalls/Cleaner.test.js
@@ -20,46 +20,58 @@ describe('Cleaner', () => {
   })
 
   it('randomFactCleaner should call get with url', () => {
-
+    Call.randomFactCleaner(url)
+    expect(fetchCall.get).toHaveBeenCalledWith(url)
   })
 
   it('randomFactCleaner should return Batman', async () => {
-
+    fetchCall.get = jest.fn(() => ({data:'Batman'}))
+    expect(await Call.randomFactCleaner(url)).toEqual('Batman')
   })
 
   it('randomAdviceCleaner should call get with url', () => {
-    
+    Call.randomAdviceCleaner(url)
+    expect(fetchCall.get).toHaveBeenCalledWith(url)
   })
 
   it('randomAdviceCleaner should return Batman', async () => {
-
+    fetchCall.get = jest.fn(() => ({slip: {advice:'Batman'}}))
+    expect(await Call.randomAdviceCleaner(url)).toEqual('Batman')
   })
 
   it('randomJokeCleaner should call get with url', () => {
-    
+    Call.randomJokeCleaner(url)
+    expect(fetchCall.get).toHaveBeenCalledWith(url)
   })
 
   it('randomJokeCleaner should return Batman', async () => {
-
+    fetchCall.get = jest.fn(() => ({joke:'Batman'}))
+    expect(await Call.randomJokeCleaner(url)).toEqual('Batman')
   })  
 
   it('searchAdviceCleaner should call get with url', () => {
-    
+    Call.searchAdviceCleaner(url)
+    expect(fetchCall.get).toHaveBeenCalledWith(url)
   })
 
   it('searchAdviceCleaner should return Batman', async () => {
-
+    fetchCall.get = jest.fn(() => ({slips: [{advice:'Batman'}]}))
+    expect(await Call.searchAdviceCleaner(url)).toEqual('Batman')
   }) 
 
   it('searchJokeCleaner should call get with url', () => {
-    
+    Call.searchJokeCleaner(url)
+    expect(fetchCall.get).toHaveBeenCalledWith(url)
   })
 
   it('searchJokeCleaner should return Batman', async () => {
-
+    fetchCall.get = jest.fn(() => ({results:[{joke:'Batman'}]}))
+    expect(await Call.searchJokeCleaner(url)).toEqual('Batman')
   }) 
 
-  it('getRandom should return a number between 1 and 10', () => {
-
+  it('getRandom should return a number between 0 and 9', () => {
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    expect(Call.getRandom(array)).toBeGreaterThanOrEqual(1)
+    expect(Call.getRandom(array)).toBeLessThanOrEqual(10)
   })
 })
