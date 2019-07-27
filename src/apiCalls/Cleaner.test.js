d08f18d07ea0e9f010f02a3454d8f5e9de2758e9
Nathan <nathan.froeh@gmail.com>
2019-07-27 14:58:50 -0600

    Add factOfTheDayCleaner return test

--- src/apiCalls/Cleaner.test.js
+++ src/apiCalls/Cleaner.test.js
@@ -5,11 +5,41 @@ import {fetchCall} from './ApiCalls';
 describe('Cleaner', () => {
   const url = 'https://batman.com';
 
-  it('factOfTheDayCleaner should call get with url', () => {
+  beforeEach(() => {
     fetchCall.get = jest.fn()
+  })
+
+  it('factOfTheDayCleaner should call get with url', () => {
     Call.factOfTheDayCleaner(url)
     expect(fetchCall.get).toHaveBeenCalledWith(url)
   })
 
+  it('factOfTheDayCleaner should return Batman', async () => {
+    fetchCall.get = jest.fn(() => ({text:'Batman'}))
+    expect(await Call.factOfTheDayCleaner(url)).toEqual('Batman')
+  })
 
+  it('randomFactCleaner should call get with url', () => {
+
+  })
+
+  it('randomAdviceCleaner should call get with url', () => {
+    
+  })
+
+  it('randomJokeCleaner should call get with url', () => {
+    
+  })
+
+  it('searchAdviceCleaner should call get with url', () => {
+    
+  })
+
+  it('searchJokeCleaner should call get with url', () => {
+    
+  })
+
+  it('getRandom should return a number between 1 and 10', () => {
+
+  })
 })
