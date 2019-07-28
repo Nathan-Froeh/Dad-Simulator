c84f2a624f76c7f6b56a4d663009c1b3b84f0312
Nathan <nathan.froeh@gmail.com>
2019-07-28 08:19:20 -0600

    Add randomFactsReducer tests

---
+++ src/Reducers/randomFactsReducer.test.js
@@ -0,0 +1,19 @@
+import {randomFactsReducer} from './randomFactsReducer'
+
+describe('randomFactsReducer', () => {
+  const action = {
+    type: 'RANDOM_FACT',
+    payload: 'Batman'
+  }
+  it('should return Batman', () => {
+    expect(randomFactsReducer('', action)).toEqual('Batman')
+  })
+
+  it('should return state', () => {
+    const action = {
+      type: '',
+      payload: 'Batman'
+    }
+    expect(randomFactsReducer('', action)).toEqual('')
+  })
+})
