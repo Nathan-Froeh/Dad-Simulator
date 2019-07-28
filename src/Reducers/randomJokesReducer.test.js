ae3d97e2a6df5f003f0524a4144af8339185d7e6
Nathan <nathan.froeh@gmail.com>
2019-07-28 08:20:39 -0600

    Add randomJokesReducer tests

---
+++ src/Reducers/randomJokesReducer.test.js
@@ -0,0 +1,19 @@
+import {randomJokesReducer} from './randomJokesReducer'
+
+describe('randomJokesReducer', () => {
+  const action = {
+    type: 'RANDOM_JOKES',
+    payload: 'Batman'
+  }
+  it('should return Batman', () => {
+    expect(randomJokesReducer('', action)).toEqual('Batman')
+  })
+
+  it('should return state', () => {
+    const action = {
+      type: '',
+      payload: 'Batman'
+    }
+    expect(randomJokesReducer('', action)).toEqual('')
+  })
+})
