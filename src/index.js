2161a72138af1289ced1c763137f8f305ee8915d
Nathan <nathan.froeh@gmail.com>
2019-07-29 10:40:55 -0600

    Ignore index test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './Containers/App/App';
-import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import allReducers from './Reducers/index';
@@ -20,4 +19,3 @@ ReactDOM.render(
 </Provider>
   , document.getElementById('root'));
 
-serviceWorker.unregister();
